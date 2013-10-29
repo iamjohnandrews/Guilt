920f67634959347b6190b0b010e6b0e2b1896e07
John Andrews <johnandrews@Johns-MacBook.local>
2013-10-29 11:52:45 -0500

    Added a TableViewController and made a new class for it. Added segue from button to new TableViewController.

---
+++ Guilt/ImagesViewController.h
@@ -0,0 +1,13 @@
+//
+//  ImagesViewController.h
+//  Guilt
+//
+//  Created by John Andrews on 10/29/13.
+//  Copyright (c) 2013 John Andrews. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImagesViewController : UITableViewController
+
+@end
