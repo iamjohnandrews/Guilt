6f87fc6310ccc36f6d3ab9edc7cfd92a3e60312f
John Andrews <johnnydrews@gmail.com>
2014-05-11 09:04:37 -0400

    add parse and twitter constants

---
+++ Guilt/Constants.h
@@ -0,0 +1,18 @@
+//
+//  Constants.h
+//  Guilt
+//
+//  Created by John Andrews on 5/11/14.
+//  Copyright (c) 2014 John Andrews. All rights reserved.
+//
+
+#pragma mark - Parse Constants
+
+extern NSString * const PARSE_APPLICATION_ID;
+extern NSString * const PARSE_CLIENT_KEY;
+
+#pragma mark - Twitter Constants
+
+extern NSString * const TWITTER_CONSUMER_KEY;
+extern NSString * const TWITTER_CONSUMER_SECRET;
+
---
+++ Guilt/Constants.m
@@ -0,0 +1,17 @@
+//
+//  Constants.m
+//  Guilt
+//
+//  Created by John Andrews on 5/11/14.
+//  Copyright (c) 2014 John Andrews. All rights reserved.
+//
+
+#import "Constants.h"
+
+#pragma mark - Parse Constants
+NSString * const PARSE_APPLICATION_ID = @"HMDmp3Y7ihXmaRXw5rfjSszBTwa7I0Uc3Rl7DNZu";
+NSString * const PARSE_CLIENT_KEY = @"BUHidRvb464SnFmDyh0qgZ6qrL15gKI1NSUP0LLk";
+
+#pragma mark - Twitter Constants
+NSString * const TWITTER_CONSUMER_KEY = @"6HGYzIN6t2qJILEN0BlAwB0XQ";
+NSString * const TWITTER_CONSUMER_SECRET = @"eqip3xLUZI2Th1kZpji9Uv8MmAH7LI012aMvwjlWLZOZ6a3F7Q";
