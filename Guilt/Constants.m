937ad019839dbd9855fa45c2ed324ef57fcb0646
John Andrews <johnnydrews@gmail.com>
2014-06-16 19:40:52 -0400

    add new Parse info

--- Guilt/Constants.m
+++ Guilt/Constants.m
@@ -9,8 +9,12 @@
 #import "Constants.h"
 
 #pragma mark - Parse Constants
-NSString * const PARSE_APPLICATION_ID = @"HMDmp3Y7ihXmaRXw5rfjSszBTwa7I0Uc3Rl7DNZu";
-NSString * const PARSE_CLIENT_KEY = @"BUHidRvb464SnFmDyh0qgZ6qrL15gKI1NSUP0LLk";
+NSString * const PARSE_APPLICATION_ID = @"HLhF6HXu0oFgSFEX37Esotnk7McBgvehN2q8YIvt";
+NSString * const PARSE_CLIENT_KEY = @"uHZUFoJoyhqr0x2hYrXmJTTffUSCw6K2hKWM4JCI";
+
+//JR Info
+//NSString * const PARSE_APPLICATION_ID = @"HMDmp3Y7ihXmaRXw5rfjSszBTwa7I0Uc3Rl7DNZu";
+//NSString * const PARSE_CLIENT_KEY = @"BUHidRvb464SnFmDyh0qgZ6qrL15gKI1NSUP0LLk";
 
 #pragma mark - Twitter Constants
 NSString * const TWITTER_CONSUMER_KEY = @"6HGYzIN6t2qJILEN0BlAwB0XQ";
